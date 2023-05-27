94821d30138ef728309b07ff985b484487e3b518
WispySparks <wispysparks@gmail.com>
2023-05-26 19:17:26 -0500

    working continually get screenshots

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
+#include <synchapi.h>
+#include <windef.h>
 #include <windows.h>
+#include <wingdi.h>
 
-int main(int argc, char* argv[]) {
+namespace {
     const int width = 1280;
     const int height = 720;
-    HDC displayContext = CreateDC("DISPLAY", NULL, NULL, NULL);
-    HDC memoryDisplayContext = CreateCompatibleDC(displayContext);
-    HBITMAP memoryBitmap = CreateCompatibleBitmap(memoryDisplayContext, width, height);
-    SelectObject(memoryDisplayContext, memoryBitmap);
-    BitBlt(memoryDisplayContext, 0, 0, width, height, displayContext, 0, 0, CAPTUREBLT);
-    BitBlt(displayContext, 0, 0, width, height, memoryDisplayContext, 0, 0, CAPTUREBLT);
-    DeleteObject(memoryBitmap);
-    DeleteDC(memoryDisplayContext);
-    DeleteDC(displayContext);
-    std::cout << "---END---";
+}
+
+int main(int argc, char* argv[]) {
+    HDC screen = GetDC(NULL);
+    HDC memoryScreen = CreateCompatibleDC(screen); // Place in memory that we're gonna copy the actual screen to
+    HBITMAP bitmap = CreateCompatibleBitmap(screen, width, height);
+    SelectObject(memoryScreen, bitmap);
+
+    while (true) {
+        // Copy the screen to our memory that currently has a bitmap
+        BitBlt(memoryScreen, 0, 0, width, height, screen, 0, 0, SRCCOPY);
+        EmptyClipboard();
+        OpenClipboard(NULL);
+        SetClipboardData(CF_BITMAP, bitmap);
+        CloseClipboard();
+        std::cout << "Copied screenshot to clipboard!\n";
+        Sleep(1000);
+    }
+
+    DeleteObject(bitmap);
+    DeleteDC(memoryScreen);
+    DeleteDC(screen);
+    std::cout << "---PROGRAM END---";
     return 0;
 }
