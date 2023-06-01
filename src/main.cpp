51a34a67a15b8a264c619732816b8f23de6f1108
WispySparks <wispysparks@gmail.com>
2023-05-31 21:20:52 -0500

    writing files

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <synchapi.h>
-#include <windef.h>
+#include <string>
 #include <windows.h>
-#include <wingdi.h>
 
 namespace {
     const int width = 1280;
     const int height = 720;
+    const std::string basePath = "C:\\Users\\wispy\\ProgrammingProjects\\C++\\ScreenCapture\\output\\image-";
 }
 
 int main(int argc, char* argv[]) {
@@ -14,16 +13,18 @@ int main(int argc, char* argv[]) {
     HDC memoryScreen = CreateCompatibleDC(screen); // Place in memory that we're gonna copy the actual screen to
     HBITMAP bitmap = CreateCompatibleBitmap(screen, width, height);
     SelectObject(memoryScreen, bitmap);
-
+    int i = 0;
     while (true) {
         // Copy the screen to our memory that currently has a bitmap
         BitBlt(memoryScreen, 0, 0, width, height, screen, 0, 0, SRCCOPY);
-        EmptyClipboard();
-        OpenClipboard(NULL);
-        SetClipboardData(CF_BITMAP, bitmap);
-        CloseClipboard();
-        std::cout << "Copied screenshot to clipboard!\n";
+        std::string filePath = basePath + std::to_string(i) + ".txt";
+        HANDLE file = CreateFile(filePath.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+        std::string data = "data!!!";
+        WriteFile(file, &data, data.length(), NULL, NULL);
+        CloseHandle(file);
+        std::cout << "Wrote file: " << filePath << "\n";
         Sleep(1000);
+        i++;
     }
 
     DeleteObject(bitmap);
