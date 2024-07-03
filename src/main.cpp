bb9aa66184829b11edbe9dd8c1a0ff4da1bc2c9c
WispySparks <wispysparks@gmail.com>
2024-07-03 01:55:59 -0500

    trying to read data

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,22 @@
 
 void CaptureDesktop();
 void CaptureWindow();
+std::string GetCommand(std::string pixelFormat);
 
 namespace {
 const int width = GetSystemMetrics(SM_CXVIRTUALSCREEN);
 const int height = GetSystemMetrics(SM_CYVIRTUALSCREEN);
-const std::string cmd = std::format(
-    "ffmpeg -hide_banner -y -f rawvideo -pix_fmt bgr24 -s {}x{} -i - -c:v "
-    "libx264 -pix_fmt yuv420p -r 30 -an out_vid.mp4",
-    width, height);
+
 }
 
 int main(int argc, char* argv[]) {
     HandleError(width == 0, "GetSystemMetrics(Width) failed!");
     HandleError(height == 0, "GetSystemMetrics(Height) failed!");
-    // https://stackoverflow.com/a/43631781
     // https://stackoverflow.com/questions/76567120/capturing-a-window-on-win-10-without-winrt
+    // https://github.com/microsoft/DirectXTK/blob/main/Src/ScreenGrab.cpp
+    // Move to ComPtr, using Microsoft::WRL::ComPtr;, #include <wrl/client.h>
+    // Test if need flush and clear state for swapchain
+    // should also maybe unmap the texture
     // CaptureDesktop();
     CaptureWindow();
     std::cout << "---PROGRAM END---\n\n";
@@ -65,7 +66,7 @@ void CaptureWindow() {
     DXGI_SWAP_CHAIN_DESC1 swapChainDesc;
     swapChainDesc.Width = 0;
     swapChainDesc.Height = 0;
-    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
     swapChainDesc.Stereo = FALSE;
     swapChainDesc.SampleDesc = {1, 0};
     swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
@@ -89,7 +90,24 @@ void CaptureWindow() {
     D3D11_MAPPED_SUBRESOURCE mappedResource;
     hr = context->Map(copiedFrame, 0, D3D11_MAP_READ, 0, &mappedResource);
     HandleError(hr, "Couldn't map the frame buffer!", cleanup);
+
+    const int size = mappedResource.RowPitch * textureDesc.Height;
+    std::vector<uint8_t> buffer(static_cast<uint8_t*>(mappedResource.pData),
+                                static_cast<uint8_t*>(mappedResource.pData) + size);
+    auto data = (char*)mappedResource.pData;
+    for (int i = 0; i < 5000; i++) {
+        std::cout << static_cast<int>(*data);
+        ++data;
+        // std::cout << +*(char*)mappedResource.pData;
+        // ++((char*)mappedResource.pData);
+    }
+    // FILE* pipe = _popen(GetCommand("rgba").c_str(), "wb");
+    // const int size = mappedResource.RowPitch * textureDesc.Height;
+    // while (!GetAsyncKeyState(VK_RSHIFT)) {
+    //     std::fwrite(mappedResource.pData, sizeof(char), size, pipe);
+    // }
     // Cleanup
+    // std::fclose(pipe);
     cleanup();
 }
 
@@ -106,7 +124,7 @@ void CaptureDesktop() {
     std::vector<char> buffer(
         std::abs(infoHeader.biWidth * infoHeader.biHeight * (infoHeader.biBitCount / CHAR_BIT)));
     // Bitmaps are stored as BGR, BI_RGB simply means uncompressed data.
-    FILE* pipe = _popen(cmd.c_str(), "wb");
+    FILE* pipe = _popen(GetCommand("bgr24").c_str(), "wb");
     while (!GetAsyncKeyState(VK_RSHIFT)) {
         HGDIOBJ prevObj = SelectObject(destDC, bitmap);
         HandleError(prevObj == NULL, "SelectObject(Bitmap) failed!");
@@ -124,4 +142,11 @@ void CaptureDesktop() {
     DeleteObject(bitmap);
     DeleteDC(destDC);
     ReleaseDC(handle, srcDC);
+}
+
+std::string GetCommand(std::string pixelFormat) {
+    return std::format(
+        "ffmpeg -hide_banner -y -f rawvideo -pix_fmt {} -s {}x{} -i - -c:v "
+        "libx264 -pix_fmt yuv420p -r 30 -an out_vid.mp4",
+        pixelFormat, width, height);
 }
