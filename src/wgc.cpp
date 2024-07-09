a8ef157b2427a1ce36e493ee5983150235e6ccf4
WispySparks <wispysparks@gmail.com>
2024-07-09 03:04:50 -0500

    frames are arriving

--- src/wgc.cpp
+++ src/wgc.cpp
@@ -1,31 +1,38 @@
 #include <d3d11.h>
 #include <windows.graphics.directx.direct3d11.interop.h>
+#include <winrt/windows.foundation.h>
 #include <winrt/windows.graphics.capture.h>
 
 #include <iostream>
 
+#include "util.hpp"
+
 namespace winrt {
 using namespace ::Windows::Graphics::DirectX::Direct3D11;
-using namespace Windows::Graphics::Capture;
-using namespace Windows::Graphics::DirectX;
-using namespace Windows::Graphics::DirectX::Direct3D11;
+using namespace winrt::Windows::Foundation;
+using namespace winrt::Windows::Graphics::Capture;
+using namespace winrt::Windows::Graphics::DirectX;
+using namespace winrt::Windows::Graphics::DirectX::Direct3D11;
 }
 
-using winrt::check_hresult;
 using winrt::com_ptr;
 
-void OnFrameArrived(winrt::Direct3D11CaptureFramePool const& framePool,
-                    IInspectable const& inspectable) {
+void OnFrameArrived(const winrt::Direct3D11CaptureFramePool& framePool,
+                    const winrt::IInspectable&) {
+    std::cout << "Frame Arrived!\n";
     winrt::Direct3D11CaptureFrame frame = framePool.TryGetNextFrame();
     winrt::IDirect3DSurface rtSurface = frame.Surface();
     com_ptr<IDXGISurface> surface;
     com_ptr<winrt::IDirect3DDxgiInterfaceAccess> dxgiInterfaceAccess{
         rtSurface.as<winrt::IDirect3DDxgiInterfaceAccess>()};
-    dxgiInterfaceAccess->GetInterface(IID_PPV_ARGS(&surface));
+    HRESULT hr = dxgiInterfaceAccess->GetInterface(IID_PPV_ARGS(&surface));
+    HandleError(hr, "Couldn't get IDXGISurface!");
     DXGI_SURFACE_DESC desc;
-    surface->GetDesc(&desc);
+    hr = surface->GetDesc(&desc);
+    HandleError(hr, "Couldn't get DXGI_SURFACE_DESC!");
     DXGI_MAPPED_RECT data;
-    surface->Map(&data, DXGI_MAP_READ);
+    hr = surface->Map(&data, DXGI_MAP_READ);
+    HandleError(hr, "Couldn't map DXGI_MAPPED_RECT!");
 
     const int size = data.Pitch * desc.Height;
     std::vector<BYTE> buffer(data.pBits, data.pBits + size);
@@ -37,8 +44,10 @@ void OnFrameArrived(winrt::Direct3D11CaptureFramePool const& framePool,
 }
 
 void CaptureWindowWGC(HWND window) {
+    winrt::GraphicsCaptureAccess::RequestAccessAsync(winrt::GraphicsCaptureAccessKind::Borderless)
+        .get();
     com_ptr<ID3D11Device> device{};
-    com_ptr<IDXGIDevice> dxgiDevice{};
+    com_ptr<IDXGIDevice> idxgiDevice{};
     winrt::IDirect3DDevice iDevice{};
     winrt::DirectXPixelFormat pixelFormat{winrt::DirectXPixelFormat::B8G8R8A8UIntNormalized};
     winrt::GraphicsCaptureItem item =
@@ -46,13 +55,20 @@ void CaptureWindowWGC(HWND window) {
     std::wcout << item.DisplayName().c_str() << "\n";
     UINT deviceFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
     deviceFlags |= D3D11_CREATE_DEVICE_DEBUG;  // DEBUG FLAG
-    check_hresult(D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, deviceFlags, NULL, 0,
-                                    D3D11_SDK_VERSION, device.put(), NULL, NULL));
-    check_hresult(CreateDirect3D11DeviceFromDXGIDevice(
-        dxgiDevice.get(), reinterpret_cast<IInspectable**>(winrt::put_abi(iDevice))));
+    HRESULT hr = D3D11CreateDevice(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, deviceFlags, NULL, 0,
+                                   D3D11_SDK_VERSION, device.put(), NULL, NULL);
+    HandleError(hr, "Couldn't create ID3D11Device!");
+    hr = device->QueryInterface(idxgiDevice.put());
+    HandleError(hr, "Couldn't create IDXGIDevice!");
+    hr = CreateDirect3D11DeviceFromDXGIDevice(idxgiDevice.get(),
+                                              reinterpret_cast<IInspectable**>(&iDevice));
+    HandleError(hr, "Couldn't create IDirect3DDevice!");
     winrt::Direct3D11CaptureFramePool framePool =
-        winrt::Direct3D11CaptureFramePool::Create(iDevice, pixelFormat, 1, item.Size());
+        winrt::Direct3D11CaptureFramePool::CreateFreeThreaded(iDevice, pixelFormat, 1, item.Size());
     framePool.FrameArrived(&OnFrameArrived);
     winrt::GraphicsCaptureSession session = framePool.CreateCaptureSession(item);
+    session.IsBorderRequired(false);
     session.StartCapture();
+    while (!GetAsyncKeyState(VK_RSHIFT)) {
+    }
 }
